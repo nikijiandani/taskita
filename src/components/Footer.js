69126093b415e880c9d493e0aad54455e006551c
Nikita Jiandani <niki.jiandani@gmail.com>
2019-10-20 12:22:43 -0400

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,23 +4,7 @@ export default function Footer() {
   return (
     <footer>
       <p>
-        Coded with lots of
-        <span role='img' aria-label='love'>
-          ❤️
-        </span>
-        and
-        <span role='img' aria-label='coffee'>
-          ☕️
-        </span>
-        by
-        <a
-          href='https://github.com/nikijiandani'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Nikita Jiandani
-        </a>
-        .
+        <span>© Taskita 2019</span>
       </p>
     </footer>
   );
